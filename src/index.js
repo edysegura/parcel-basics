70df2c579928c5f866d6af7977eb8d0c45f94340
Edy Segura <ednardo@inatel.br>
2018-02-23 14:24:42 -0300

    Importing only the needed modules

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import Rx from 'rxjs/Rx'
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
+
+import { Observable } from 'rxjs/Observable';
 
 const input = document.querySelector('input')
 const output = document.querySelector('output')
 const oneSecond = 1000
 
-const $input = Rx.Observable
+const $input = Observable
   .fromEvent(input, 'keyup')
   .map(event => event.currentTarget.value)
   .debounceTime(oneSecond)
