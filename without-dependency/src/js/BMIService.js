76afd38b0c1e300f5e8050f5bf9f276d7f1bacf6
Edy Segura <ednardo@inatel.br>
2018-04-26 23:43:10 -0300

    Change indentation to 2 spaces

--- without-dependency/src/js/BMIService.js
+++ without-dependency/src/js/BMIService.js
@@ -1,10 +1,8 @@
 export default class BMIService {
-
-    static getIndex(weight, height) {
-        if (typeof (weight) === 'number' && typeof (height) === 'number') {
-            return weight / (height * height)
-        }
-        return null
+  static getIndex(weight, height) {
+    if (typeof weight === 'number' && typeof height === 'number') {
+      return weight / (height * height);
     }
-
+    return null;
+  }
 }
