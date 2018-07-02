abe4368ecd0f80cbed59baf48d9c5a19918655ab
Edy Segura <edysegura@gmail.com>
2018-07-02 08:21:18 -0300

    Refactoring RxJS example

--- using-rxjs/src/index.js
+++ using-rxjs/src/index.js
@@ -1,16 +1,15 @@
-import 'rxjs/add/observable/fromEvent';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/fromEvent'
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/map'
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable'
 
 const input = document.querySelector('input')
 const output = document.querySelector('output')
 const oneSecond = 1000
 
-const $input = Observable
-  .fromEvent(input, 'keyup')
+const userInput = Observable.fromEvent(input, 'keyup')
   .map(event => event.currentTarget.value)
   .debounceTime(oneSecond)
 
-$input.subscribe(text => output.textContent = text)
+userInput.subscribe(text => (output.textContent = text))
