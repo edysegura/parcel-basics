f4bd8a4b0f668a205c691a051371955d99bc21f3
Edy Segura <edysegura@gmail.com>
2019-05-27 23:43:44 -0300

    Add textarea example

--- using-dotenv/src/js/index.js
+++ using-dotenv/src/js/index.js
@@ -2,3 +2,6 @@
 
 const span = document.querySelector('span')
 span.textContent = process.env.API_ENDPOINT || 'not loaded'
+
+const textarea = document.querySelector('textarea')
+textarea.value = process.env.PATH.replace(/;/g, '\n')
