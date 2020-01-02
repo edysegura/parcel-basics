6504867238b9adc7eedb6807ca480d21e79e1d2a
Edy Segura <edysegura@gmail.com>
2020-01-02 15:11:33 -0300

    Rename js folder to ts

--- using-typescript/src/ts/guest.service.ts
+++ using-typescript/src/ts/guest.service.ts
@@ -2,7 +2,7 @@ import { guestData as data } from '../assets/guest-data'
 import { Guest } from './guest.model'
 
 export class GuestService {
-  list: Guest[]
+  private list: Guest[]
 
   constructor() {
     this.loadData()
@@ -19,10 +19,11 @@ export class GuestService {
   private mapToGuest(item: any): Guest {
     const guest: Guest = {
       id: item.id,
-      name: `${item.firstName} ${item.lastName}`,
+      name: `${ item.firstName } ${ item.lastName }`,
       email: item.email,
       gender: item.gender
     }
+
     return guest
   }
 }
