4cb7f9764ff6d3514bb674a35e0e379fbb2eed7b
Edy Segura <edysegura@users.noreply.github.com>
2021-08-11 00:27:17 +0000

    fix: add support to linux

--- using-dotenv/src/js/index.js
+++ using-dotenv/src/js/index.js
@@ -4,4 +4,4 @@ const span = document.querySelector('span')
 span.textContent = process.env.API_ENDPOINT || 'not loaded'
 
 const textarea = document.querySelector('textarea')
-textarea.value = process.env.PATH.replace(/;/g, '\n')
+textarea.value = process.env.PATH.replace(/;|:/g, '\n')
